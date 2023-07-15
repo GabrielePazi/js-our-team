1dac0e2a1546292eacd01bb6f142795bb21bb28d
GabrielePazi <pazigabriele@gmail.com>
2023-07-15 13:15:09 +0200

    1.1 esercizio svolto fino a bonus 2

--- script/main.js
+++ script/main.js
@@ -1,38 +1,46 @@
 'use strict'
 
+const cardsArray = document.querySelectorAll("img");
+const cardsTitles = document.querySelectorAll(".card-title");
+const cardsTexts = document.querySelectorAll(".card-text")
+
 let team =[
   {
     name: "Wayne Barnett",
     occupation: "Founder & CEO",
-    profilePhoto: "wayne-barnett-founder-ceo.jpg"
+    profilePhoto: "./img/wayne-barnett-founder-ceo.jpg"
   },
   {
     name: "Angela Caroll",
     occupation: "Chief Editor",
-    profilePhoto: "angela-caroll-chief-editor.jpg"
+    profilePhoto: "./img/angela-caroll-chief-editor.jpg"
   },
   {
     name: "Walter Gordon",
     occupation: "Office Manager",
-    profilePhoto: "walter-gordon-office-manager.jpg"
+    profilePhoto: "./img/walter-gordon-office-manager.jpg"
   },
   {
     name: "Angela Lopez",
     occupation: "Social Media Manager",
-    profilePhoto: "angela-lopez-social-media-manager.jpg"
+    profilePhoto: "./img/angela-lopez-social-media-manager.jpg"
   },
   {
     name: "Scott Estrada",
     occupation: "Developer",
-    profilePhoto: "scott-estrada-developer.jpg"
+    profilePhoto: "./img/scott-estrada-developer.jpg"
   },
   {
     name: "Barbara Ramos",
     occupation: "Graphic Designer",
-    profilePhoto: "barbara-ramos-graphic-designer.jpg"
+    profilePhoto: "./img/barbara-ramos-graphic-designer.jpg"
   }
 ]
 
 for (let i = 0; i < team.length; i++) {
   console.log(team[i].name, team[i].occupation, team[i].profilePhoto)
+
+  cardsTitles[i].innerHTML = team[i].name;
+  cardsTexts[i].innerHTML = team[i].occupation;
+  cardsArray[i].src = team[i].profilePhoto;
 }
