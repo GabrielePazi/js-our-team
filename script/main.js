c9b2c96f717e67c27951963df990b7192bcd92f0
GabrielePazi <pazigabriele@gmail.com>
2023-07-15 17:10:35 +0200

    1.2 button e form "add member"

--- script/main.js
+++ script/main.js
@@ -1,8 +1,11 @@
 'use strict'
 
-const cardsArray = document.querySelectorAll("img");
+const cardContainer = document.querySelector(".cards-row");
+const cardsImages = document.querySelectorAll("img");
 const cardsTitles = document.querySelectorAll(".card-title");
-const cardsTexts = document.querySelectorAll(".card-text")
+const cardsTexts = document.querySelectorAll(".card-text");
+const btnAdd = document.querySelector(".add-member-btn");
+let seedRandomImage = 0;
 
 let team =[
   {
@@ -37,10 +40,76 @@ let team =[
   }
 ]
 
-for (let i = 0; i < team.length; i++) {
-  console.log(team[i].name, team[i].occupation, team[i].profilePhoto)
+btnAdd.addEventListener("click", addNewMember)
 
+function addNewMember() {
+
+  //sovrascrive il numero del generatore random per avere ogni volta un'immagine randomica diversa
+  seedRandomImage++
+
+  createNewCard()
+
+  //prende di nuovo questi valori perchè ora c'è una card in più e i valori globali non sono aggiornati
+  const cardsImagesNew = document.querySelectorAll("img");
+  const cardsTitlesNew = document.querySelectorAll(".card-title");
+  const cardsTextsNew = document.querySelectorAll(".card-text");
+
+  //prendo i valori di nome, lavoro e foto profilo dagli input
+  const memberName = document.querySelector(".new-member-name").value;
+  const memberOccupation = document.querySelector(".new-member-job").value;
+  const memberImage = `https://picsum.photos/seed/${seedRandomImage}picsum/200`
+
+  const newMember = createMember(memberName, memberOccupation, memberImage);
+
+  //inserice la il nuovo oggetto persona creato nell'array di oggetti "team" 
+  team.push(newMember);
+
+  //aggiorna i dati dell'ultima card aggiunta(se no di default sono uguali alla penultima)
+  for (let i = 0; i < team.length; i++) {
+
+    if (i == team.length-1) {
+      cardsTitlesNew[i].innerHTML = newMember.name;
+      cardsTextsNew[i].innerHTML = newMember.occupation;
+      cardsImagesNew[i].src = newMember.profilePhoto;
+    }
+  }
+}
+
+//aggiunge ai valori di titolo, lavoro e immagine i valori inseriti nell'array di objects team
+for (let i = 0; i < team.length; i++) {
   cardsTitles[i].innerHTML = team[i].name;
   cardsTexts[i].innerHTML = team[i].occupation;
-  cardsArray[i].src = team[i].profilePhoto;
+  cardsImages[i].src = team[i].profilePhoto;
+}
+
+/**
+ * crea un oggetto "person" impostando come nome, lavoro e foto profili quelli passati in input
+ * 
+ * @param {string} memberName 
+ * @param {string} job 
+ * @param {string} image 
+ * @returns {object}
+ */
+function createMember(memberName, job, image) {
+  let person = {
+    name: memberName,
+    occupation: job,
+    profilePhoto: image
+  }
+  return person
+}
+
+/**
+ * Clona l'ultima card e aggiunge il clone al container. Toglie la classe "last-card alla penultima carde l'aggiunge a quella appena creata"
+ * @returns 
+ */
+function createNewCard() {
+  const cardEl = document.querySelector(".last-card");
+  const newCard = cardEl.cloneNode(true);
+
+  cardContainer.append(newCard);
+
+  cardEl.classList.remove("last-card");
+  newCard.classList.add("last-card");
+  return
 }
